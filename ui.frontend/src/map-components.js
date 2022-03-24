c0768fe75fe4a158e575c71a2bce4419dd5efaef
Pablo Barral <pablo.barral.kts@gmail.com>
2022-03-24 15:21:36 -0300

    Feat: migrated select component from old project

--- ui.frontend/src/map-components.js
+++ ui.frontend/src/map-components.js
@@ -2,6 +2,7 @@ import Text from './components/Text/Text'
 import AppPage from './components/Page/AppPage'
 import CheckBox from './components/CheckboxVue/CheckBox'
 import Input from './components/InputVue/Input'
+import Select from './components/SelectVue/Select'
 import { withComponentMappingContext, MapTo } from '@mavice/aem-vue-editable-components'
 
 // Components Mapping
@@ -9,3 +10,4 @@ MapTo('vue/components/text')(Text)
 MapTo('vue/components/page')(withComponentMappingContext(AppPage))
 MapTo('vue/components/checkbox-vue')(CheckBox)
 MapTo('vue/components/input-vue')(Input)
+MapTo('vue/components/select-vue')(Select)
