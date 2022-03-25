890271ab0c2ebf34d8e7ce1d41f7ede548d4d443
Rafel <rafaeloliveira63@yahoo.com>
2022-03-25 17:28:19 -0300

    Feat: adding the text componet

--- ui.frontend/src/components/CustonText/TextVue.vue
+++ ui.frontend/src/components/CustonText/TextVue.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="checkbox-vue" id="ckeckbox">
-    <input type="checkbox" :name="field" />
-    <label :for="field">{{ field }}</label>
-  </div>
+  <div class="text-vue">
+    <p>{{text}}</p>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'checkboxVue',
+  name: 'TextVue',
   props: {
-    field: {
+    text: {
       type: String,
       default: 'Full Name'
     }
--- ui.frontend/src/map-components.js
+++ ui.frontend/src/map-components.js
@@ -3,6 +3,7 @@ import AppPage from './components/Page/AppPage'
 import CheckBox from './components/CheckboxVue/CheckBox'
 import Input from './components/InputVue/Input'
 import Select from './components/SelectVue/Select'
+import TextVue from './components/CustonText/TextVue'
 import { withComponentMappingContext, MapTo } from '@mavice/aem-vue-editable-components'
 
 // Components Mapping
@@ -11,3 +12,4 @@ MapTo('vue/components/page')(withComponentMappingContext(AppPage))
 MapTo('vue/components/checkbox-vue')(CheckBox)
 MapTo('vue/components/input-vue')(Input)
 MapTo('vue/components/select-vue')(Select)
+MapTo('vue/components/text-vue')(TextVue)
