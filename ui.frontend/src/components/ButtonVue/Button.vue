bd1c94de36cb95640b7ec269fc69f5206438a827
Pablo Barral <pablo.barral.kts@gmail.com>
2022-03-25 20:39:05 -0300

    Feat: added custom button component

---
+++ ui.frontend/src/components/ButtonVue/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="button-vue">
+        <button>{{field}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'buttonVue',
+  props: {
+    field: {
+      type: String,
+      default: 'Button Text'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import './index.scss';
+</style>
