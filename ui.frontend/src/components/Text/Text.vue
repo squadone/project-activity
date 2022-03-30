c0163dc2df468e70949c2d1a06d05cfacb417fa9
Rafel <rafaeloliveira63@yahoo.com>
2022-03-30 14:10:03 -0300

    Feat: finalizing the success page structure

--- ui.frontend/src/components/Text/Text.vue
+++ ui.frontend/src/components/Text/Text.vue
@@ -31,6 +31,17 @@ export default {
       type: String
     }
   },
+  updated () {
+    const elementThirdBox = document.querySelector('[cqpath="/content/vue/vue/trird-tab/jcr:content/root/responsivegrid/responsivegrid_25683"] div')
+    elementThirdBox.style.backgroundColor = '#fff'
+    elementThirdBox.style.height = '76vh'
+    elementThirdBox.style.display = 'flex'
+    elementThirdBox.style.flexDirection = 'column'
+    elementThirdBox.style.borderRadius = '20px'
+    elementThirdBox.style.margin = '2% auto'
+    elementThirdBox.style.width = '70%'
+    elementThirdBox.style.justifyContent = 'space-between'
+  },
   computed: {
     modelId () {
       return extractModelId(this.cqPath)
